f1f023ac2c83ef87cabd146f844e067d6e197aba
danishzahid <danishst.johns@gmail.com>
2023-11-22 01:08:08 +0530

    first commit - setup backend

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
