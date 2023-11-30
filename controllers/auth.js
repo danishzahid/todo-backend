759faa3a8f455cb39f3d133968bc6f6484d2e61e
danishzahid <danishst.johns@gmail.com>
2023-11-30 23:19:28 +0530

    added:domain in res.cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,6 +41,7 @@ export const login = async (req, res, next) => {
     return res
       .cookie("access_token", token, {
         httpOnly: true,
+        domain: "https://todo-frontend-danishzahid.vercel.app",
       })
       .status(200)
       .json({ name: user.name, email: user.email, message: "login success" });
